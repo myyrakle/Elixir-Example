66d6b97b8d507351ba3910de824841c30b8a0b58
myyrakle <sssang97@naver.com>
2021-03-05 01:52:28 +0900

    Update Enum.ex

--- src/Enum.ex
+++ src/Enum.ex
@@ -14,3 +14,7 @@ nums = [1, 2, 3, 4, 5]
 nums = Enum.filter(nums, fn(e)->e>2 end)
 str = Enum.join(nums, ", ")
 IO.puts str
+
+nums = [1, 2, 3, 4, 5]
+sum = Enum.reduce(nums, 0, fn(e, acc)->acc+e end)
+IO.puts sum
