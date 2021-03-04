993a298eca1b2e457f92f4852531d1863888a02f
myyrakle <sssang97@naver.com>
2021-03-05 01:44:17 +0900

    Update Enum.ex

--- src/Enum.ex
+++ src/Enum.ex
@@ -4,3 +4,8 @@ IO.puts str
 
 nums = [1, 2, 3, 4, 5]
 Enum.each(nums, fn(e)->IO.puts e end)
+
+nums = [1, 2, 3, 4, 5]
+nums = Enum.map(nums, fn(e)->e*2 end)
+str = Enum.join(nums, ", ")
+IO.puts str
